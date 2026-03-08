54cce07bed8a6982be5f9edc365354a50882c575
BrodyReiter <brody.reiter@gmail.com>
2026-03-08 17:56:04 -0400

    Update R13_FIO08_J.java
    compliant code

--- R13_FIO08_J.java
+++ R13_FIO08_J.java
@@ -1,6 +1,6 @@
 FileInputStream in = new FileInputStream("file");
-byte data;
-while ((data = (byte) in.read()) != -1) {
+int data;
+while ((data = in.read()) != -1) {
     // Process data
 }
 in.close();
