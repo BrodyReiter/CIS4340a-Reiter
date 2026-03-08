6aafa716fbbd5c63ed085a47da7c9f306f8842a6
BrodyReiter <brody.reiter@gmail.com>
2026-03-08 17:56:24 -0400

    Update R14_SER05_J.java
    compliant code

--- R14_SER05_J.java
+++ R14_SER05_J.java
@@ -1,6 +1,6 @@
 public class OuterSer implements Serializable {
     private int rank;
-    class InnerSer implements Serializable {
+    static class InnerSer implements Serializable {
         protected String name;
     }
 }
